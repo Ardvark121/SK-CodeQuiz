ab023c1896f8a9d261b7f1d9f4a84d6492834f59
Shawn Kim <kimshawnj@gmail.com>
2023-07-17 03:01:22 -0500

    Added first Question

--- Assests/script.js
+++ Assests/script.js
@@ -1,5 +1,18 @@
-var timer = document.querySelector("#time");
-var starter = document.querySelector("#Startbutton");
+const timer = document.querySelector("#time");
+const starter = document.querySelector("#Startbutton");
+const QuestArray = [
+  'What is the correct JavaScript syntax to write "Hello World"?',
+];
+const Answers1 = [
+  'response.write("Hello World")',
+  '"Hello World"',
+  'document.write("Hello World")',
+  '("Hello World")',
+];
+var quiz = document.querySelector("#Questions");
+var options = document.querySelector("#Options");
+
+var a = 1;
 
 var count = 0;
 
@@ -8,6 +21,23 @@ function Counter() {
     count++;
     timer.textContent = "Timer:" + count;
   }, 1000);
+  switchques();
+  starter.hidden = true;
 }
+
+function switchques() {
+  Answers = Answers1;
+  for (var i = 0; i < Answers.length; i++) {
+    var optionli = document.createElement("li");
+    var optionbut = document.createElement("button");
+    optionbut.innerHTML = Answers[i];
+    optionli.append(optionbut);
+    options.append(optionli);
+    if (i == 0) {
+      quiz.innerHTML = QuestArray[i];
+    }
+  }
+}
+function checkquest() {}
 
 starter.addEventListener("click", Counter);
